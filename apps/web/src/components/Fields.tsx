b9e858cb4ad32df9308f31613177a9d70aaf50ec
Kenny-Gin1 <kenny.chung@live.se>
2022-10-16 03:09:24 +0200

    cleaning and styling

--- apps/web/src/components/Fields.tsx
+++ apps/web/src/components/Fields.tsx
@@ -1,21 +1,22 @@
-import { Text, Input, Button } from '@mantine/core';
+import { Text, Input, Button, Box } from '@mantine/core';
 import { useState} from "react"
 
 const Fields = ({ item, addOrEdit, condition, i }) => {
     const [toggle, setToggle] = useState(false)
     const toggleSymbol = toggle ? "false" : "true"
     return item.type === 'boolean' ? (
-    <>
-      <Text style={{ fontSize: 14 }}>
-        {item.title && item.title.length > 2 ? item.title : item.name}
-      </Text>
+    <Box style={{
+        display: 'flex',
+        flexDirection: 'column',
+    }}>
       <Button
+      style={{width: '70%', margin: 'auto'}}
         onClick={() => {
           setToggle(!toggle)
           addOrEdit(item, toggleSymbol, 'field_id_' + i, condition)
         }}
-      >Toggle</Button>
-    </>
+      >{item.title && item.title.length > 2 ? item.title : item.name}</Button>
+      </Box>
   ) : (
     <>
       <Text style={{ fontSize: 14 }}>
